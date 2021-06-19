f4d80440b723d25e4ef1134e64ebf63bdcc72673
gorpher <gorpher101@gmail.com>
2021-06-19 23:13:43 +0800

    formatBytesString

--- os.go
+++ os.go
@@ -1,7 +1,9 @@
 package gone
 
 import (
+	"github.com/shopspring/decimal"
 	"net"
+	"strconv"
 	"strings"
 )
 
@@ -20,4 +22,111 @@ func MacAddr() (upMac []string, err error) {
 		}
 	}
 	return upMac, nil
+}
+
+const (
+	Bytes = 1
+	KB    = Bytes << 10 // 1 KB = 1024 Bytes
+	MB    = Bytes << 20 // 1 MB = 1048576 Bytes
+	GB    = Bytes << 30 // 1 GB = 1073741824 Bytes
+	TB    = Bytes << 40 // 1 TB = 1099511627776 Bytes
+	PB    = Bytes << 50 // 1 PB = 1125899906842624 Bytes
+	EB    = Bytes << 60 // 1 EB = 1152921504606846976 Bytes
+)
+
+// FormatBytesStringOhMyGod 格式化存储大小，理论上应该使用该方式格式化存储大小，但是实际上不是这样的，呜呜呜呜呜呜。。。。
+func FormatBytesStringOhMyGod(b int) string {
+	if b < KB {
+		return strconv.Itoa(b) + " bytes"
+	}
+	if b == KB {
+		return "1 KB"
+	}
+	if b < MB {
+		return formatBytesUnit(b, KB, " KB")
+	}
+	if b == MB {
+		return "1 MB"
+	}
+	if b < GB {
+		return formatBytesUnit(b, MB, " MB")
+	}
+	if b == GB {
+		return "1 GB"
+	}
+	if b < TB {
+		return formatBytesUnit(b, GB, " GB")
+	}
+	if b == TB {
+		return "1 TB"
+	}
+	if b < PB {
+		return formatBytesUnit(b, TB, " TB")
+	}
+	if b == PB {
+		return "1 PB"
+	}
+	if b < EB {
+		return formatBytesUnit(b, PB, " PB")
+	}
+	if b == EB {
+		return "1 EB"
+	}
+	return formatBytesUnit(b, EB, " EB")
+}
+
+const (
+	kb = 1000
+	mb = 1_000_000
+	gb = 1_000_000_000
+	tb = 1_000_000_000_000
+	pb = 1_000_000_000_000_000
+	eb = 1_000_000_000_000_000_000
+)
+
+// FormatBytesString 格式化bytes单位成可阅读单位形式,由于电脑制造商使用的是1000为单位计算磁盘大小
+// 所以基本上使用该函数格式化存储大小
+func FormatBytesString(b int) string {
+	if b < kb {
+		return strconv.Itoa(b) + " bytes"
+	}
+	if b == kb {
+		return "1 KB"
+	}
+	if b < mb {
+		return formatBytesUnit(b, kb, " KB")
+	}
+	if b == mb {
+		return "1 MB"
+	}
+	if b < gb {
+		return formatBytesUnit(b, mb, " MB")
+	}
+	if b == gb {
+		return "1 GB"
+	}
+	if b < tb {
+		return formatBytesUnit(b, gb, " GB")
+	}
+	if b == tb {
+		return "1 TB"
+	}
+	if b < pb {
+		return formatBytesUnit(b, tb, " TB")
+	}
+	if b == pb {
+		return "1 PB"
+	}
+	if b < eb {
+
+		return formatBytesUnit(b, pb, " PB")
+	}
+	if b == eb {
+		return "1 EB"
+	}
+	return formatBytesUnit(b, eb, " EB")
+}
+
+func formatBytesUnit(a, b int, suffix string) string {
+	return decimal.NewFromInt(int64(a)).DivRound(decimal.NewFromInt(int64(b)), 2).Truncate(2).String() + suffix
 }
--- os_test.go
+++ os_test.go
@@ -1,10 +1,63 @@
 package gone
 
-import "testing"
+import (
+	"math/rand"
+	"os"
+	"path/filepath"
+	"testing"
+)
 
 func TestMacAddr(t *testing.T) {
 	if _, err := MacAddr(); err != nil {
 		t.Error(err)
 		return
 	}
+}
+
+func TestFormatBytesString(t *testing.T) {
+	t.Log(KB, MB, GB, TB, PB, EB)
+	t.Log(kb, mb, gb, tb, pb, eb)
+	dir, _ := os.Getwd()
+	dir = filepath.Join(dir, "tmp")
+	var tests = []struct {
+		Name string
+		Size int
+		Res  string
+	}{
+		{Name: "900b", Size: 900 * Bytes, Res: "900 bytes"},
+		{Name: "1020b", Size: 1020 * Bytes, Res: "1.02 KB"},
+		{Name: "900kb", Size: 900 * KB, Res: "921.6 KB"},
+		{Name: "1000kb", Size: 1000 * KB, Res: "1.02 MB"},
+		{Name: "1029kb", Size: 1029 * KB, Res: "1.05 MB"},
+		{Name: "900MB", Size: 900 * MB, Res: "943.72 MB"},
+		{Name: "1000MB", Size: 1000 * MB, Res: "1.05 GB"},
+		{Name: "1020MB", Size: 1020 * MB, Res: "1.07 GB"},
+	}
+	for i := range tests {
+		b := make([]byte, tests[i].Size)
+		_, err := rand.Read(b)
+		if err != nil {
+			t.Error(err)
+		}
+		s := FormatBytesString(tests[i].Size)
+		if s != tests[i].Res {
+			t.Errorf("%s format err : get %s ,but want %s", tests[i].Name, s, tests[i].Res)
+		}
+		//t.Run(tests[i].Name, func(t *testing.T) {
+		//	temp, err := os.CreateTemp(dir, r2)
+		//	if err != nil {
+		//		t.Error(err)
+		//	}
+		//	t.Log(tests[i].Name, r)
+		//	temp.Write(b)
+		//	defer temp.Close()
+		//})
+	}
+}
+func BenchmarkFormatBytesString(b *testing.B) {
+	b.ResetTimer()
+	for i := EB; i > 0; i-- {
+		FormatBytesString(i)
+	}
+	b.StopTimer()
 }
