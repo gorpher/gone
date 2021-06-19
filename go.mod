21a05089a39ecabb5606cbb45839a4eec41c8e03
gorpher <gorpher101@gmail.com>
2021-06-19 23:13:18 +0800

    retry

--- go.mod
+++ go.mod
@@ -1,12 +1,13 @@
 module github.com/gorpher/gone
 
-go 1.12
+go 1.16
 
 require (
 	github.com/bwmarrin/snowflake v0.3.0
-	github.com/google/go-cmp v0.4.0
+	github.com/google/go-cmp v0.4.1
 	github.com/rs/xid v1.3.0
 	github.com/satori/go.uuid v1.2.0
+	github.com/shopspring/decimal v1.2.0
 	github.com/tjfoc/gmsm v1.4.0
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
---
+++ retry_test.go
@@ -0,0 +1,61 @@
+package gone
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestRetrySuccess(t *testing.T) {
+	cnt := 0
+	err := Retry(3, 1*time.Nanosecond, func() error {
+		if cnt == 0 {
+			cnt++
+			return fmt.Errorf("retry")
+		}
+		return nil
+	})
+
+	if err != nil {
+		t.Errorf("error should be nil, %s", err)
+	}
+
+	if cnt != 1 {
+		t.Errorf("cnt should be 1, %d", cnt)
+	}
+}
+
+func TestRetryFail(t *testing.T) {
+	cnt := 0
+	err := Retry(4, 1*time.Nanosecond, func() error {
+		cnt++
+		return fmt.Errorf("retry")
+	})
+
+	if err == nil || err.Error() != "retry" {
+		t.Errorf("error should be occured")
+	}
+
+	if cnt != 4 {
+		t.Errorf("cnt should be 4, but %d", cnt)
+	}
+}
+
+func TestWithContext(t *testing.T) {
+	cnt := 0
+	ctx, cancel := context.WithTimeout(context.Background(), 150*time.Millisecond)
+	defer cancel()
+	err := WithContext(ctx, 3, 100*time.Millisecond, func() error {
+		cnt++
+		return fmt.Errorf("retry")
+	})
+
+	if err != context.DeadlineExceeded {
+		t.Errorf("error should be %s, %s", context.DeadlineExceeded, err)
+	}
+
+	if cnt != 2 {
+		t.Errorf("cnt should be 2, %d", cnt)
+	}
+}
