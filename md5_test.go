019202f4ffa4515082781fd6f0cdc4fb0ccf76a7
gorpher <gorpher101@gmail.com>
2021-08-09 09:40:23 +0800

    md5

--- md5_test.go
+++ md5_test.go
@@ -8,7 +8,23 @@ func TestMD5Encrypt(t *testing.T) {
 	text := RandBytes(100)
 	salt := RandBytes(10)
 
-	actual := MD5Encrypt(text, salt)
+	actual, err := MD5(text, salt)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if actual == "" {
+		t.Error("MD5 error")
+	}
+}
+
+func TestMD5EncryptBytes(t *testing.T) {
+	text := RandBytes(100)
+	salt := RandBytes(10)
+
+	actual, err := MD5Encrypt(text, salt)
+	if err != nil {
+		t.Fatal(err)
+	}
 	if actual == "" {
 		t.Error("MD5Encrypt error")
 	}
